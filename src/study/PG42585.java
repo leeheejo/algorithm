74930e8028256eacb3214689e07b9906c6a4e3bd
leeheejo <jo4610jo@naver.com>
2020-05-27 08:32:50 +0900

    200527

---
+++ src/study/PG42585.java
@@ -0,0 +1,32 @@
+package study;
+
+import java.util.Stack;
+
+public class PG42585 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String arrangement = "()(((()())(())()))(())";
+		int ans = solution(arrangement);
+		System.out.println(ans);
+
+	}
+
+	public static int solution(String arrangement) {
+		int answer = 0;
+		Stack<Character> stack = new Stack<Character>();
+
+		for (int i = 0; i < arrangement.length(); i++) {
+			if (arrangement.charAt(i) == '(')
+				stack.add(arrangement.charAt(i));
+			else {
+				stack.pop();
+				if (arrangement.charAt(i - 1) == '(')
+					answer += stack.size();
+				else
+					answer++;
+			}
+		}
+		return answer;
+	}
+}
