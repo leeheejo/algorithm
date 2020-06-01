75e8145cbd0980baf2196701d1af7a7bf531120f
leeheejo <jo4610jo@naver.com>
2020-06-01 21:45:26 +0900

    200601

--- src/study/programmers/N42626.java
+++ src/study/programmers/N42626.java
@@ -1,4 +1,4 @@
-package study;
+package study.programmers;
 
 import java.util.PriorityQueue;
 
--- src/study/programmers/PG42629_2.java
+++ src/study/programmers/PG42629_2.java
@@ -1,10 +1,10 @@
-package study;
+package study.programmers;
 
 import java.util.Comparator;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class PG42629 {
+public class PG42629_2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
