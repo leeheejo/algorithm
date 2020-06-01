71a15ecffe05779a5c07123abc0e1eb7f582f099
leeheejo <jo4610jo@naver.com>
2020-06-02 08:21:32 +0900

    200601

---
+++ src/study/PG42627.java
@@ -0,0 +1,16 @@
+package study;
+
+public class PG42627 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[][] jobs = { { 0, 3 }, { 1, 9 }, { 2, 6 } };
+		System.out.println(solution(jobs));
+	}
+
+	public static int solution(int[][] jobs) {
+		int answer = 0;
+		return answer;
+	}
+
+}
